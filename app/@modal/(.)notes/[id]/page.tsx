cdeba4faea129fa4459a471884ae3dedf855f3fa
OlegVins <olegvinnickii@gmail.com>
2026-04-20 22:44:12 +0200

    replace layout/filter

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import Modal from '@/components/Modal/Modal';
-import NotePreview from '@/app/@modal/(.)notes/[id]/NotePreview';
+import NotePreview from '@/app/@modal/(.)notes/[id]/NotePreview.client';
 
 export default function ModalPreview() {
     const router = useRouter();
